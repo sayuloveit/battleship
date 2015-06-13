d79f38c9ed4de7580937739c89c4a7da309a6f4d
vic <vicxizhu@gmail.com>
2015-06-12 22:27:35 -0400

    add ship classes

--- model/ship.rb
+++ model/ship.rb
@@ -1,8 +1,45 @@
 class Ship
   attr_reader :hullpoints
 
-  def initialize(hullpoints)
+  def initialize(hullpoints = nil)
     @hullpoints = hullpoints
+  end
+end
+
+
+class Carrier < Ship
+
+  def initialize(hullpoints = 5)
+    super
+  end
+end
+
+class Battleship < Ship
+
+  def initialize(hullpoints = 4)
+    super
+  end
+end
+
+class Cruiser < Ship
+
+  def initialize(hullpoints = 3)
+    super
+  end
+end
+
+class Submarine < Ship
+
+  def initialize(hullpoints = 3)
+    super
+  end
+
+end
+
+class Destroyer < Ship
+
+  def initialize(hullpoints = 2)
+    super
   end
 
 end
--- runner.rb
+++ runner.rb
@@ -1,3 +1,5 @@
 require_relative './model/ship'
 require_relative './model/board'
 
+
+
