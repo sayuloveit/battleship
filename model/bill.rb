dc7dc0c3b47e955074f5c9eb88b41477830ce1cf
vic <vicxizhu@gmail.com>
2015-06-13 20:23:27 -0400

    initialize with available moves

--- model/bill.rb
+++ model/bill.rb
@@ -4,12 +4,21 @@ class Bill < Board
   attr_reader :moves
   def initialize
     super
+    @avaiable_moves = @board.keys.select { |coord| valid_coord?(coord) }
     @moves = Array.new
+    @hits = Array.new
   end
 
   def bill_target(coordinate)
     @moves.push(coordinate)
     target(coordinate)
   end
+
+
+  private
+
+  # def
+
+  # end
 
 end
