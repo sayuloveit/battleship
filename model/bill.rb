1b1dee7a39c2cc272b33f8c55c367faa07cee728
vic <vicxizhu@gmail.com>
2015-06-14 18:23:49 -0400

    add simple logic to determine next moves after a hit

--- model/bill.rb
+++ model/bill.rb
@@ -2,73 +2,83 @@ require_relative 'board'
 require 'pry'
 
 class Bill < Board
-  attr_reader :possible_moves, :last_hit, :last_move, :hits, :priority_targets
+  attr_reader :possible_moves, :priority_targets, :last_hit
 
   def initialize
     super
     @possible_moves = @board.keys.select { |coord| valid_coord?(coord) }.shuffle
     @last_hit = nil
-    @last_move = nil
-    @hits = []
     @priority_targets = []
+    @hunting = false   #detemines when to stop using priority targets
   end
 
+  # gives a target and remove it from possible moves
   def give_target
-    if @hits.length == 1      #targets area surrounding hit
-      @priority_targets = surrounding_coordinates(@hits[0]).shuffle
-      @possible_moves.delete(@priority_targets.shift)
-    elsif @hits.length >= 2   #makes next hit
-      @possible_moves.delete(next_hit(@hits[-2], @hits[-1]))
+    if @hunting
+      target = @priority_targets.shift
+      @possible_moves.delete(target)
     else
-      @last_move = @possible_moves.shift
+      @possible_moves.shift
     end
   end
 
 
-  def response(hullpoints_left)
-    if hullpoints_left == 0
-      @possible_moves -= @hits
-      @priority_targets.clear
-      @hits.clear
-    elsif !!hullpoints_left
-      binding.pry
-      @last_hit = @last_move
-      @hits.push(@last_hit)
-    else
-      nil
-    end
+  def response(last_target, hullpoints_left)
+      if hullpoints_left == 0
+        @hunting = false
+        @priority_targets.clear
+      elsif !!hullpoints_left
+        @hunting = true
+        if @priority_targets.empty?
+          @last_hit = last_target
+          @priority_targets = keen_targets(@last_hit, hullpoints_left).shuffle
+        # else
+        #   @last_hit = last_target
+        end
+      end
   end
 
   # private
 
-  #determines next coordinates to target based on hits
-  # def priority_targets
-  #   if @hits.length == 1
-  #     @priority_targets = surrounding_coordinates(@hits[0])
-  #   elsif @hits.length >= 2
-  #     next_hit(@hits[0], @hits[1])
-  #   end
-  # end
+  # narrow down coordinates to hit based on info from last hit
+  def keen_targets(hit_coordinate, hullpoints_left)
+    row = hit_coordinate[0]
+    col = hit_coordinate[1].to_i
+    result = []
 
-  def surrounding_coordinates(coordinate)
-    x = coordinate[0]
-    y = coordinate[1].to_i
+    result.concat(n_direction_coordinates(hit_coordinate, hullpoints_left, 'u'))
+    result.concat(n_direction_coordinates(hit_coordinate, hullpoints_left, 'd'))
+    result.concat(n_direction_coordinates(hit_coordinate, hullpoints_left, 'l'))
+    result.concat(n_direction_coordinates(hit_coordinate, hullpoints_left, 'r'))
 
-    ["#{x}#{y - 1}", "#{x.succ}#{y}", "#{x}#{y + 1}", "#{previous_letter(x)}#{y}"].select { |coord| valid_coord?(coord) }.map(&:to_sym)
+    result.select{ |coord| valid_coord?(coord) }.map(&:to_sym)
   end
 
   def previous_letter(letter)
     'a'.upto(letter).to_a[-2]
   end
 
-  #calc next hit based on last two hits
-  #todos: account for cases where it targets off the board
-  def next_hit(hit1, hit2)
-    if hit1[0] == hit2[0]
-      hit1[1] < hit2[1] ? "#{hit2[0]}#{hit2[1].to_i + 1}".to_sym : "#{hit2[0]}#{hit2[1].to_i - 1}".to_sym
-    else
-      hit1[0] < hit2[0] ? "#{hit2[0].succ}#{hit2[1]}".to_sym : "#{previous_letter(hit2[0])}#{hit2[1]}".to_sym
+  # determines next coordinates base on direction
+  def next_coordinate(coordinate, direction)
+    row = coordinate[0]
+    col = coordinate[1].to_i
+    case direction
+      when 'l'            #left
+        "#{row}#{col - 1}"
+      when 'r'            #right
+        "#{row}#{col + 1 }"
+      when 'u'            #up
+        "#{previous_letter(row)}#{col}"
+      when 'd'            #down
+        "#{row.succ}#{col}"
     end
+  end
+
+  # generates next n coordinates based on starting coordinate and direction
+  def n_direction_coordinates(coordinate, n, direction)
+    result = [next_coordinate(coordinate, direction)]
+    (n - 1).times { result << next_coordinate(result[-1], direction) }
+    result
   end
 
 
