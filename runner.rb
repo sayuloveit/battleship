908a8d7a9e423039cddc16a474d61fac110fdc51
vic <vicxizhu@gmail.com>
2015-06-22 22:36:29 -0400

    turn controller into a class

--- runner.rb
+++ runner.rb
@@ -2,29 +2,86 @@ require_relative 'model/ship'
 require_relative 'model/board'
 require_relative 'model/bill'
 
-def symbolify(input)
-  input.to_sym
+class BattleShip
+
+  def initialize(args = {})
+    @player_ships = args.fetch(:player_ships)
+    @opponent = args.fetch(:opponent)
+    @player = args.fetch(:board_obj)
+    run
+  end
+
+  def run
+    player_setup
+    turns
+  end
+
+
+  def player_setup
+    until @player_ships.empty?
+      puts "Your Board"
+      puts @player
+
+      current_ship = @player_ships.shift
+
+      begin
+        puts "Please give a location and alignment to place your #{current_ship.class} (#{current_ship.hullpoints} spaces)"
+        print "Location: "
+        location = symbolify(gets.chomp)
+        print "Alignment(h = horizonal, v = verticle): "
+        alignment = gets.chomp
+      end until @player.place_ship(current_ship, location, alignment)
+    end
+
+  end
+
+  def turns
+    until @player.game_over? || @opponent.game_over?
+
+      puts "Bill's Board"
+      puts @opponent
+      puts '-' * 10
+      puts "Your Board"
+      puts @player
+
+      print "pick a target: "
+      input = gets.chomp
+      @opponent.target(symbolify(input))
+
+
+      winner = "You" if @opponent.game_over?
+
+
+      puts "Bill is thinking..."
+      sleep 1
+
+      bills_target = @opponent.give_target
+      target_result = @player.target(bills_target)
+      @opponent.response(bills_target, target_result)
+      puts "bill's target: #{bills_target}"
+      puts "bill's target result: #{target_result}"
+      puts "bill's last move: #{@opponent.last_hit}"
+      puts "bill's priority targets: #{@opponent.priority_targets}"
+
+      winner = "Bill" if @player.game_over?
+
+    end
+
+    puts "#{winner} won!"
+
+  end
+
+  private
+
+  def symbolify(input)
+    input.to_sym
+  end
+
+
 end
 
-p1 = Board.new
 
-ships = [Carrier.new(player = true), Battleship.new(player = true), Cruiser.new(player = true), Submarine.new(player = true), Destroyer.new(player = true)]
-
-until ships.empty?
-  puts "Your Board"
-  puts p1
-
-  current_ship = ships.shift
-
-  begin
-    puts "Please give a location and alignment to place your #{current_ship.class} (#{current_ship.hullpoints} spaces)"
-    print "Location: "
-    location = symbolify(gets.chomp)
-    print "Alignment(h = horizonal, v = verticle): "
-    alignment = gets.chomp
-  end until p1.place_ship(current_ship, location, alignment)
-
-end
+player_ships = [Carrier.new(player = true), Battleship.new(player = true), Cruiser.new(player = true), Submarine.new(player = true), Destroyer.new(player = true)]
 
 
 bill = Bill.new
@@ -35,35 +92,5 @@ b_submarine = Submarine.new(player = true) #uncomment to see Bill's ships
 b_destroyer = Destroyer.new(player = true) #uncomment to see Bill's ships
 bill.place_ships([b_carrier, b_battleship, b_cruiser, b_submarine, b_destroyer])
 
-until p1.game_over? || bill.game_over?
 
-  puts "Bill's Board"
-  puts bill
-  puts '-' * 10
-  puts "Your Board"
-  puts p1
-
-  print "pick a target: "
-  input = gets.chomp
-  bill.target(symbolify(input))
-
-
-  winner = "You" if bill.game_over?
-
-
-  puts "Bill is thinking..."
-  sleep 1
-
-  bills_target = bill.give_target
-  target_result = p1.target(bills_target)
-  bill.response(bills_target, target_result)
-  puts "bill's target: #{bills_target}"
-  puts "bill's target result: #{target_result}"
-  puts "bill's last move: #{bill.last_hit}"
-  puts "bill's priority targets: #{bill.priority_targets}"
-
-  winner = "Bill" if p1.game_over?
-
-end
-
-puts "#{winner} won!"
+BattleShip.new({player_ships: player_ships, opponent: bill, board_obj: Board.new})
