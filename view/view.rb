4254354418941fe7eb4dcb172c64bf7d480712aa
vic <vicxizhu@gmail.com>
2015-06-24 11:02:34 -0400

    fix display_board

--- view/view.rb
+++ view/view.rb
@@ -5,13 +5,12 @@ module View
   BOARD_HEIGHT = 8
 
   def self.display_board(board_str)
-    rows = ('A'..'Z').to_a[0..BOARD_HEIGHT]
+    rows = ('A'...'Z').to_a[0..BOARD_HEIGHT]
     cols = (1..26).to_a[0...BOARD_WIDTH]
-
-    display = board_str.chars.each_slice(BOARD_WIDTH).to_a.map do |row|
+    display = board_str.split("\n").map(&:chars).map do |row|
       row.unshift(rows.shift)
-    end.map(&:join).join("\n")
-    # binding.pry
+    end.map(&:join).push(" #{(1..BOARD_WIDTH).to_a.join}").join("\n")
+
     puts display
   end
 end
